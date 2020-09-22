691776a8ec82e61eff27241050b6c675ec194b5a
Jani Basha S <john2468basha@gmail.com>
2020-09-22 16:20:13 +0530

    Add files via upload

---
+++ code/get_started.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Downloads raw data into ./download
+# and saves preprocessed data into ./data
+# Get directory containing this script
+
+CODE_DIR=`pwd`
+
+export PYTHONPATH=$PYTHONPATH:$CODE_DIR
+
+pip install -r requirements.txt
+
+# download punkt, perluniprops
+if [ ! -d "/usr/local/share/nltk_data/tokenizers/punkt" ]; then
+    python2 -m nltk.downloader punkt
+fi
+
+
+# SQuAD preprocess is in charge of downloading
+# and formatting the data to be consumed later
+DATA_DIR=data
+DOWNLOAD_DIR=download
+mkdir -p $DATA_DIR
+rm -rf $DATA_DIR
+python2 preprocessing/squad_preprocess.py
+
+# Download distributed word representations
+python2 preprocessing/dwr.py
+
+# Data processing for TensorFlow
+python2 qa_data.py --glove_dim 100
